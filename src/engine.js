31a3c7c557eadacf6520fb6ece3a63c9ba295003
Ali Cem Özkara <41231109+alicemist@users.noreply.github.com>
2023-09-24 00:00:17 +0300

    Update engine.js
    Vector2 sınıfındaki multiple ve divade metodlarında aynı isimle iki farklı fonksiyon var. Bunu overloading gibi görebilirsiniz, ancak JavaScript'te bu doğrudan desteklenmez. 
    
    requestAnimationFrame kullanarak etkin bir oyun döngüsü  olusturuldu

--- src/engine.js
+++ src/engine.js
@@ -1,148 +1,144 @@
-//import { Graphics } from "./graphics";
+/**
+ * KairaEngine: A basic 2D game engine.
+ */
 
-var KairaEngine = {};
+var KairaEngine = {
+    deltaTime: 1 / 30
+};
 
-KairaEngine.deltaTime = 1/30;
-KairaEngine.update = function(){
+/**
+ * Main update loop for KairaEngine.
+ */
+KairaEngine.update = function() {
+    // Update logic for the engine goes here
+};
 
-}
-
-KairaEngine.Component = class {
-    update(Projectile = KairaEngine.Projectile){
-
-    }
-}
-
+/**
+ * A 2D vector class with basic arithmetic operations.
+ */
 KairaEngine.Vector2 = class {
-    constructor(x = 0, y = 0){
+    constructor(x = 0, y = 0) {
         this.x = x;
         this.y = y;
     }
-    add(a = Vector2){
+
+    add(a) {
         return new KairaEngine.Vector2(this.x + a.x, this.y + a.y);
     }
-    subtract(a = Vector2){
+
+    subtract(a) {
         return new KairaEngine.Vector2(this.x - a.x, this.y - a.y);
     }
-    
-    multiple(a = Vector2){
+
+    multipleByVector(a) {
         return new KairaEngine.Vector2(this.x * a.x, this.y * a.y);
     }
-    multiple(a = 0){
+
+    multipleByScalar(a = 1) {
         return new KairaEngine.Vector2(this.x * a, this.y * a);
     }
 
-    divade(a = Vector2){
+    divideByVector(a) {
         return new KairaEngine.Vector2(this.x / a.x, this.y / a.y);
     }
-    divade(a = 0){
+
+    divideByScalar(a = 1) {
         return new KairaEngine.Vector2(this.x / a, this.y / a);
     }
-    distance(a = Vector2){
-        var dis = Math.sqrt(Math.abs(this.x - a.x) + Math.abs(this.y - a.y));
-        return dis;
-    }
-}
 
+    distance(a) {
+        return Math.sqrt((this.x - a.x) ** 2 + (this.y - a.y) ** 2);
+    }
+};
+
+/**
+ * Base component class for game objects.
+ */
+KairaEngine.Component = class {
+    update(Projectile = KairaEngine.Projectile) {
+        // Default update method for components
+    }
+};
+
+/**
+ * Transform component that holds position and scale of game objects.
+ */
 KairaEngine.Transform = class extends KairaEngine.Component {
-    constructor(position = KairaEngine.Vector2, scale = KairaEngine.Vector2){
+    constructor(position = new KairaEngine.Vector2(), scale = new KairaEngine.Vector2()) {
         super();
         this.position = position;
         this.scale = scale;
     }
-}
+};
 
-//#region glgraphics
-//KairaEngine.Graphics = new Graphics();
-//#endregion
-
-KairaEngine.Projectile = class{
-    constructor(transform = KairaEngine.Transform, components = []){
+/**
+ * Main game object or entity class.
+ */
+KairaEngine.Projectile = class {
+    constructor(transform = new KairaEngine.Transform(), components = []) {
         this.transform = transform;
         this.components = components;
     }
-    addComponent(component){
+
+    addComponent(component) {
         this.components.push(component);
-        return this.components.component;
+        return this.components[this.components.length - 1];
     }
-    getComponent(component){
-        this.components.forEach(element => {
-            if (typeof(component) == typeof(element)){
-                return element;
-            }
-        });
+
+    getComponent(componentType) {
+        return this.components.find(component => component instanceof componentType);
     }
-    update(){
-        this.components.forEach(element => {
-            element.Projectile = this;
-            element.update(this);
+
+    update() {
+        this.components.forEach(component => {
+            component.update(this);
         });
-        //#region glgraphics
-        /*
-        KairaEngine.Graphics.setShaders();
-        KairaEngine.Graphics.setBuffers({
-            position: {
-                numberOfComponents: 3, // X and Y ordered pair coordinates
-                data: new Float32Array([
-                    (0.0 * this.transform.scale.x) + this.transform.position.x, (0.5 * this.transform.scale.y) + this.transform.position.y, 0,
-                    (-0.5 * this.transform.scale.x) + this.transform.position.x, (-0.5 * this.transform.scale.y) + this.transform.position.y, 0,
-                    (0.5 * this.transform.scale.x) + this.transform.position.x, (-0.5 * this.transform.scale.y) + this.transform.position.y, 0,
-                ])
-            },
-            color: { 
-                numberOfComponents: 3, // RGB triple
-                data: new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1])
-            }
-        });
-        KairaEngine.Graphics.use();
-        */
-        //#endregion
+        // Additional update logic for the Projectile class goes here
     }
-}
+};
+
+/**
+ * Rigidbody component for physics simulation.
+ */
 KairaEngine.Rigidbody = class extends KairaEngine.Component {
-    constructor(Projectile = KairaEngine.Projectile,mass = 1,velocity = new KairaEngine.Vector2, accelation = new KairaEngine.Vector2){
+    constructor(Projectile = new KairaEngine.Projectile(), mass = 1, velocity = new KairaEngine.Vector2(), acceleration = new KairaEngine.Vector2()) {
         super();
         this.mass = mass;
         this.Projectile = Projectile;
         this.velocity = velocity;
-        this.accelation = accelation;
-        this.Collider = {};
-        
+        this.acceleration = acceleration;
     }
-    update(Projectile = KairaEngine.Projectile){
-        
-        this.Projectile = Projectile;
-        
-        console.log(this.accelation);
-        
-        this.Projectile.transform.position = this.Projectile.transform.position.add(this.velocity.multiple(KairaEngine.deltaTime));
-        this.velocity = this.velocity.add(this.accelation.multiple(KairaEngine.deltaTime));
-        
-        
-        
-    }
-}
 
+    update(Projectile) {
+        this.Projectile = Projectile;
+        this.Projectile.transform.position = this.Projectile.transform.position.add(this.velocity.multipleByScalar(KairaEngine.deltaTime));
+        this.velocity = this.velocity.add(this.acceleration.multipleByScalar(KairaEngine.deltaTime));
+    }
+};
+
+/**
+ * Sphere collider component for basic collision detection.
+ */
 KairaEngine.Collider = {};
 KairaEngine.Collider.SphereCollider = class extends KairaEngine.Component {
-    constructor(Projectile = new KairaEngine.Projectile,radius = 1){
+    constructor(Projectile = new KairaEngine.Projectile(), radius = 1) {
         super();
         this.Projectile = Projectile;
         this.radius = radius;
     }
-    update(Projectile){
-        this.Projectile = Projectile;
-    }
-    onCollisionEnter(collider = KairaEngine.Collider.SphereCollider){
-        //console.log((this.radius/2)+(collider.radius/2));
-        //console.log(this.Projectile.transform.position.distance(collider.Projectile.transform.position));
-        console.log(collider);
-        if (this.Projectile.transform.position.distance(collider.Projectile.transform.position) <= (this.radius/2)+(collider.radius/2)){
-            return true;
-        }
-        else return false;
-    }
-}
 
-var KairaEditor = {};
+    onCollisionEnter(collider) {
+        return this.Projectile.transform.position.distance(collider.Projectile.transform.position) <= (this.radius / 2) + (collider.radius / 2);
+    }
+};
 
+/**
+ * Main game loop using requestAnimationFrame for smooth updates.
+ */
+KairaEngine.run = function() {
+    KairaEngine.update();
+    requestAnimationFrame(KairaEngine.run);
+};
+
+// Starting the game loop
+KairaEngine.run();
